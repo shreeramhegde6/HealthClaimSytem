f3e2f380ef81ab7f777a6a5d89447b7445c0c7cc
shreeramhegde6 <46131900+shreeramhegde6@users.noreply.github.com>
2023-03-09 11:41:37 +0530

    Add files via upload

---
+++ Controllers/ClaimController.cs
@@ -0,0 +1,49 @@
+﻿using HealthClaimApp.Models;
+using HealthClaimApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthClaimApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClaimController : ControllerBase
+    {
+
+        IClaimService claimService;
+        public ClaimController(IClaimService _claimService)
+        {
+            claimService = _claimService;
+        }
+
+        [HttpGet]
+        [Route("GetAllClaimType")]
+        public IEnumerable<TblClaimType> GetAllClaimType()
+        {
+            var claimTypeList = claimService.GetAllClaimType();
+            return claimTypeList;
+        }
+
+        [HttpPost]
+        [Route("add-claim")]
+        public IActionResult SaveClaim([FromBody] TblClaim claim)
+        {
+            try
+            {
+                var data = claimService.SaveClaim(claim);
+                return Ok(data);
+
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex);
+
+            }
+
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,72 @@
+﻿using HealthClaimApp.Models;
+using HealthClaimApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthClaimApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+
+        HealthclaimAppContext db;
+        private IConfiguration _config;
+        ILoginService loginService;
+
+        public LoginController(ILoginService _loginService)
+        {
+            loginService = _loginService;
+        }
+
+        [HttpPost]
+        [Route("login-user")]
+        public IActionResult Login(TblLogin user)
+        {
+            try
+            {
+                IActionResult response = Unauthorized();
+                var userdata = loginService.AuthenticateUser(user, false);
+                if (user != null)
+                {
+                    var tokenString = loginService.GenerateToken(userdata);
+                    response = Ok(new { token = tokenString, role = userdata.UserRole });
+                }
+                return response;
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex);
+            }
+
+        }
+        [HttpPost]
+        [Route("register-user")]
+        public IActionResult Register([FromBody] TblLogin user)
+        {
+            try
+            {
+                IActionResult response = Unauthorized();
+                var userdata = loginService.AuthenticateUser(user, true);
+                if (user != null)
+                {
+                    var tokenString = loginService.GenerateToken(userdata);
+                    response = Ok(new { token = tokenString, role = userdata.UserRole });
+                }
+                return response;
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex);
+            }
+
+        }
+
+    }
+}
